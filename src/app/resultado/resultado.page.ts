555ffe045920d936f385af7b0c91ef08aea53018
felipe-maia <fe_switch@hotmail.com>
2020-03-26 16:43:09 -0300

    Mostrar resultado

--- src/app/resultado/resultado.page.ts
+++ src/app/resultado/resultado.page.ts
@@ -3,19 +3,19 @@ import { Router } from '@angular/router';
 import { Storage } from '@ionic/storage';
 
 @Component({
-  selector: 'app-pais',
+  selector: 'app-resultado',
   templateUrl: './resultado.page.html',
   styleUrls: ['./resultado.page.scss'],
 })
 export class ResultadoPage implements OnInit {
 
-  private titulo:any;
-  private msg:any;
+  private resultado:string;
+  private orientacao:string;
 
   constructor(private router: Router, public storage: Storage) { }
 
-
   ngOnInit() {
+
 
     this.storage.get('rg').then((rg) => {
       if(rg == null){
@@ -23,23 +23,16 @@ export class ResultadoPage implements OnInit {
       } 
     });
 
-    this.titulo = this.storage.get('titulo').then((titulo) => {
-      console.log('titulo é ', titulo);
-      
+  
+    this.storage.get('titulo').then((titulo) => {
+      this.resultado = titulo;
     });
 
-    this.titulo = this.storage.get('msg').then((msg) => {
-      console.log('msg é ', msg);
+    this.storage.get('msg').then((msg) => {
+      this.orientacao = msg;
     });
-
-    this.titulo = this.storage.get('titulo');
-
-    this.msg = this.storage.get('msg');
-
-    console.log("pagina resultado -> titulo ", this.titulo + " msg " + this.msg.result);
-
+    
     this.storage.clear();
 
   }
-
 }
