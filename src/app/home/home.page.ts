d8cb525ca77790bd61fd73cbe8e13d7ca4295f22
erivelton <erivelton84ribeiro@gmail.com>
2020-04-01 21:02:56 -0300

    Criação do campo termos de uso

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -11,18 +11,50 @@ import { ToastController } from '@ionic/angular';
 })
 export class HomePage {
 
-    rg: string;
-    dataNascimento: Date;
     brasaoPMPR: String = "../assets/image/pmprtransparente.png";
+
+    mostaTermo: any;
+    mostraCampoTermo: boolean = false;
+    aceitoTermo: any;
+    mostraBotaoContinua: boolean = false;
+    escondeCampoTermo: boolean = false;
   
   constructor(private router: Router, public storage:Storage, public toastCtrl: ToastController ) {
-
+    this.mostaTermo = "false";
+    this.mostraCampoTermo = false;
+    this.aceitoTermo = "false";
+    this.escondeCampoTermo = true;
   }
 
   ngOnInit() {
-
     this.storage.clear();
+  }
 
+  mostrarTermos():void{
+console.log(this.mostraCampoTermo);
+
+    if (this.mostaTermo == "true") {
+      this.mostraCampoTermo = true;
+      console.log("dentro do if: ", this.mostraCampoTermo);
+      this.escondeTermo();
+    } else {
+      this.mostraCampoTermo = false;
+    }
+  }
+
+
+  aceitarTermos():void{
+
+    if (this.aceitoTermo == "true") {
+      this.mostraBotaoContinua = true;
+      this.escondeTermo();
+    } else {
+      this.mostraBotaoContinua = false;
+    }
+  }
+
+  escondeTermo(): void{
+    this.escondeCampoTermo = false;
   }
 
   direcionaLogin(){
